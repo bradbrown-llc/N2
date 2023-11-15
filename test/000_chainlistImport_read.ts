c28b5e7e1a01a7a3c8dc2bf71808f88671b6afbf
Brad Brown <bradbrown@bradbrown.llc>
2023-11-15 15:42:07 -0500

    redo chainlist functions, slight rearrange of lib

--- test/000_chainlistImport_read.ts
+++
@@ -1,6 +0,0 @@
-import { chainlistImport } from 'N2'
-// console.log(await chainlistImport({ method: 'fetch' }))
-// console.log(await chainlistImport({ method: 'fetch', save: true }))
-// console.log(await chainlistImport({ method: 'read' }))
-console.log(await chainlistImport({ method: 'read', at: 0 }))
-// console.log(await chainlistImport({ method: 'read', target: '1700033575650' }))
